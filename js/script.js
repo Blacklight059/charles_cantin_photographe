2e347c46b9fb1610187462ed5bfd54a70e6baa5c
Steven GOMES <steven_gomes@hotmail.fr>
2023-09-10 14:27:50 +0200

    create page contact.html

--- js/script.js
+++ js/script.js
@@ -28,3 +28,34 @@ $(this).addClass("active");
 		return ".filter-button";
 	}
 });
+        /* paste this line in verbatim */
+        window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+        /* customize formbutton below*/     
+        formbutton("create", {
+          action: "https://formspree.io/f/xgejyblg",
+          title: "Comment puis-je vous aider",
+          fields: [
+            { 
+              type: "email", 
+              label: "Email:", 
+              name: "email",
+              required: true,
+              placeholder: "your@email.com"
+            },
+            {
+              type: "textarea",
+              label: "Message:",
+              name: "message",
+              placeholder: "What's on your mind?",
+            },
+            { type: "submit" }      
+          ],
+          styles: {
+            title: {
+              backgroundColor: "gray"
+            },
+            button: {
+                backgroundColor: "#7EA16B",
+            }
+          }
+        });
